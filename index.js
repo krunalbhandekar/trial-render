6f368dd937eb9538a3d87ebf423d24e3ac75e877
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2024-12-25 10:19:07 +0530

    feat: Added post route to check dynamic python code

--- index.js
+++ index.js
@@ -30,6 +30,32 @@ print("Python version:", __import__('sys').version)
   });
 });
 
+app.post("/test-python", (req, res) => {
+  const { code } = req.body;
+
+  const pythonProcess = spawn("python3", ["-c", code]);
+
+  let output = "";
+  let error = "";
+
+  pythonProcess.stdout.on("data", (data) => {
+    output += data.toString();
+    console.log(" check output", data.toString());
+  });
+
+  pythonProcess.stderr.on("data", (data) => {
+    error += data.toString();
+  });
+
+  pythonProcess.on("close", (code) => {
+    if (code === 0) {
+      res.json({ success: true, output });
+    } else {
+      res.json({ success: false, error });
+    }
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
