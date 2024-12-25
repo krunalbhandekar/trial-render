273e289c8a21dd296ee6261b8bd8de77b5f0b9a9
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2024-12-25 13:41:44 +0530

    feat: Added cpp post route to check dynamic cpp code

--- routes/cpp.js
+++ routes/cpp.js
@@ -57,4 +57,52 @@ int main() {
   }
 });
 
+router.post("/", async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    const tempFilePath = "./temp.cpp";
+    await fs.writeFile(tempFilePath, code);
+
+    const compileProcess = spawn("g++", [tempFilePath, "-o", "temp"]);
+
+    compileProcess.stdout.on("data", (data) => {
+      const outputString = data.toString();
+      console.log("Compile Output:", outputString, Date.now());
+    });
+
+    compileProcess.stderr.on("data", (data) => {
+      console.error("Compile Error:", data.toString());
+    });
+
+    compileProcess.on("close", (compileCode) => {
+      if (compileCode === 0) {
+        const execProcess = spawn("./temp");
+
+        execProcess.stdout.on("data", (data) => {
+          const outputString = data.toString();
+          console.log("Real-time Output:", outputString, Date.now());
+        });
+
+        execProcess.stderr.on("data", (data) => {
+          console.error("Execution Error:", data.toString());
+        });
+
+        execProcess.on("close", (execCode) => {
+          if (execCode === 0) {
+            console.log("C++ code executed successfully.");
+          } else {
+            console.error("C++ code execution failed.");
+          }
+        });
+      } else {
+        console.error("C++ code compilation failed.");
+      }
+    });
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false });
+  }
+});
+
 module.exports = router;
