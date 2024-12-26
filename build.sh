8a432543e43f5babc5f50d3a5dbff8a06e5fa1d7
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2024-12-26 22:06:00 +0530

    fix: Added sudo to update packages

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Updating package list..."
- apt-get update
+sudo apt-get update
 
 # Check if python3 is installed
 if ! command -v python3 &> /dev/null
 then
     echo "python3 could not be found, installing..."
-    apt-get update && apt-get install -y python3 python3-pip
+    sudo apt-get update && sudo apt-get install -y python3 python3-pip
 else
     echo "python3 is already installed"
 fi
@@ -16,7 +16,7 @@ fi
 if ! command -v python &> /dev/null
 then
     echo "python could not be found, creating symlink to python3..."
-    ln -s /usr/bin/python3 /usr/bin/python
+    sudo ln -s /usr/bin/python3 /usr/bin/python
 else
     echo "python is already installed"
 fi
@@ -25,11 +25,10 @@ fi
 if ! command -v g++ &> /dev/null
 then
     echo "g++ could not be found, installing..."
-    apt-get update && apt-get install -y g++
+    sudo apt-get update && sudo apt-get install -y g++
 else
     echo "g++ is already installed"
 fi
-
 
 # Check if Java is installed
 if ! command -v java &> /dev/null
@@ -37,7 +36,7 @@ then
     echo "Java could not be found, installing..."
 
     # Install OpenJDK (Java Development Kit)
-     apt-get install -y openjdk-11-jdk
+    sudo apt-get install -y openjdk-11-jdk
 
     # Verify installation
     echo "Java version:"
